7f315d9c2a8d27e40046fdf6930dafa7fea6c6f0
Siphosethu Samantha Mkandwana <samanthamkandwana17@gmail.com>
2023-09-11 22:58:34 +0200

    Added a search engine in JavaScript

--- src/index.js
+++ src/index.js
@@ -1,16 +1,24 @@
 function currentDate(timestamp) {
-  let date = new Date (timestamp);
+  let date = new Date(timestamp);
   let hours = date.getHours();
-  if(hours < 10) {
+  if (hours < 10) {
     hours = `0${hours}`;
   }
   let minutes = date.getMinutes();
-  if(minutes < 10) {
+  if (minutes < 10) {
     minutes = `0${minutes}`;
   }
-  let days = ["Sunday", "Monday", "Tuesday","Wensday;", "Thursday","Friday", "Saturday"];
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wensday;",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
   let day = days[date.getDay()];
-  return`${day} ${hours}:${minutes}`
+  return `${day} ${hours}:${minutes}`;
 }
 
 function showWeather(response) {
@@ -20,27 +28,37 @@ function showWeather(response) {
   console.log(response.data.temperature.current);
   console.log(response.data.condition.description);
 
-  let temperatureElement =document.querySelector("#temperature");
+  let temperatureElement = document.querySelector("#temperature");
   temperatureElement.innerHTML = Math.round(response.data.temperature.current);
 
   let cityElement = document.querySelector("#city");
   cityElement.innerHTML = response.data.city;
 
-   let descriptionElement = document.querySelector("#description");
-   descriptionElement.innerHTML = response.data.condition.description;
+  let descriptionElement = document.querySelector("#description");
+  descriptionElement.innerHTML = response.data.condition.description;
 
-   let humidityElement = document.querySelector("#humidity");
-   humidityElement.innerHTML = response.data.temperature.humidity;
+  let humidityElement = document.querySelector("#humidity");
+  humidityElement.innerHTML = response.data.temperature.humidity;
 
-   let windElement = document.querySelector("#wind");
-   windElement.innerHTML = Math.round(response.data.wind.speed); 
+  let windElement = document.querySelector("#wind");
+  windElement.innerHTML = Math.round(response.data.wind.speed);
 
-   let dateElement = document.querySelector("#date");
-   dateElement.innerHTML = currentDate(response.data.time * 1000);
+  let dateElement = document.querySelector("#date");
+  dateElement.innerHTML = currentDate(response.data.time * 1000);
 }
-let apiKey = "b19t9a07a57a44df163do01147f91d11";
-let apiUrl =
-  "https://api.shecodes.io/weather/v1/current?query=Midrand&key=b19t9a07a57a44df163do01147f91d11&units=metric";
+function search(city) {
+  let apiKey = "b19t9a07a57a44df163do01147f91d11";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
 
-console.log(apiUrl);
-axios.get(apiUrl).then(showWeather);
+  axios.get(apiUrl).then(showWeather);
+}
+function handleSubmit(event) {
+  event.preventDefault();
+  let citySearchElement = document.querySelector("#city-search");
+  search(citySearchElement.value);
+}
+
+search("Midrand");
+
+let form = document.querySelector("#search-form");
+form.addEventListener("submit", handleSubmit);
