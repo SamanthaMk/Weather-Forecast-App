927712bfca65de97a70e363c83da160ae78069df
Siphosethu Samantha Mkandwana <samanthamkandwana17@gmail.com>
2023-09-15 02:57:01 +0200

    I removed the Fahrenheit and Celsius link conversion in HTML and updated changes into JavaScript

--- src/index.js
+++ src/index.js
@@ -109,34 +109,7 @@ function handleSubmit(event) {
   search(citySearchElement.value);
 }
 
-function displayFahrenheit(event) {
-  event.preventDefault();
-  let temperatureElement = document.querySelector("#temperature");
-  celsiusLink.classList.remove("active");
-  fahrenheitLink.classList.add("active");
-  let fahrenheitTemperature = (celsiusTemperature * 9) / 5 + 32;
-  temperatureElement.innerHTML = Math.round(fahrenheitTemperature);
-}
-
-function displayCelsius(event) {
-  event.preventDefault();
-  celsiusLink.classList.add("active");
-  fahrenheitLink.classList.remove("active"); 
-let temperatureElement = document.querySelector("#temperature");
-temperatureElement.innerHTML = Math.round(celsiusTemperature);
-  
-}
-
-let celsiusTemperature = null;
-
-
 let form = document.querySelector("#search-form");
 form.addEventListener("submit", handleSubmit);
-
-let fahrenheitLink = document.querySelector("#fahrenheit-link");
-fahrenheitLink.addEventListener("click",displayFahrenheit);
-
-let celsiusLink = document.querySelector("#celsius-link");
-celsiusLink.addEventListener("click", displayCelsius);
 
 search("Midrand");
